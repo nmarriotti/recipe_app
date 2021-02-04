b16cb00a8ab35c2535fe3eb67eb3c455c4c2096d
Nick marriotti <nmarriotti@gmail.com>
2021-02-04 15:54:50 -0500

    removed noticed for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -37,7 +37,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to user_recipes_path, notice: "Recipe was successfully created." }
+        format.html { redirect_to user_recipes_path }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -50,7 +50,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to user_recipes_path, notice: "Recipe was successfully updated." }
+        format.html { redirect_to user_recipes_path }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -63,7 +63,7 @@ class RecipesController < ApplicationController
   def destroy
     @recipe.destroy
     respond_to do |format|
-      format.html { redirect_to user_recipes_path, notice: "Recipe was successfully destroyed." }
+      format.html { redirect_to user_recipes_path }
       format.json { head :no_content }
     end
   end
